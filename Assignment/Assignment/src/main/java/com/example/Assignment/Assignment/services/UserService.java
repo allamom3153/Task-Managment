7644de56aa977766d7c5800fdc8442b5aa7f862d
allamom3153 <allamom3153@gmail.com>
2024-09-27 16:04:35 +0530

    Delete Assignment/Assignment directory

--- Assignment/Assignment/src/main/java/com/example/Assignment/Assignment/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.Assignment.Assignment.services;
-
-
-import com.example.Assignment.Assignment.entity.User;
-
-public interface UserService {
-    User register(User user);
-    String login(String username, String password);
-}
